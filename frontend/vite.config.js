b124085b8da3366bfa0ca56d99ec3b0f11e98216
Sara Kiani <sara@sarakiani.com>
2026-07-12 16:07:13 -0700

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
+    test: {
+        environment: "jsdom",
+        globals: true,
+        setupFiles: "./src/test/setup.ts",
+    },
 });
